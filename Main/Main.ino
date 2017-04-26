997aab738c2c6c7aad26758fc9332d4fe6e4ac5c
duyphamduc <duypham93@gmail.com>
2017-04-26 11:59:38 -0600

    Revert "--Studdering while moving to slow down the car"
    This reverts commit f78d07bae28ea2d65720af8b08b5f5a8bbc556a7.

--- Main/Main.ino
+++ Main/Main.ino
@@ -58,8 +58,6 @@ void loop() {
   Scan();
 
   MoveDir(FORWARD);
-  delay(50);
-  MoveDir(STOP);
   if(millis() - current_time > 1000)
   {
     if(data.laserFront < laserActionThreshold)
@@ -98,12 +96,11 @@ void loop() {
 
 void TurnDeadEnd(int dir)
 {
+  delay(600);
   WriteMsg("DeadEnd Loop Start", true);
+  MoveDir(dir);
   while(true)
   {
-    MoveDir(dir);
-    delay(50);
-    MoveDir(STOP);
     Scan();
     if(data.laserFront > 400)
     {
@@ -117,12 +114,11 @@ void TurnDeadEnd(int dir)
 
 void TurnIntersection(int dir)
 {
+  delay(600);
   WriteMsg("Intersection Turn First Loop", true);
+  MoveDir(dir);
   while(true)
   {
-    MoveDir(dir);
-    delay(50);
-    MoveDir(STOP);
     Scan();
     if(data.laserFront < 400)
     {
@@ -132,9 +128,6 @@ void TurnIntersection(int dir)
   WriteMsg("Intersection Turn Second Loop", true);
   while(true)
   {
-    MoveDir(dir);
-    delay(50);
-    MoveDir(STOP);
     Scan();
     if(data.laserFront > 400)
     {
@@ -262,10 +255,10 @@ void Scan()
 {
   data.laserRight = sensor1.readRangeSingleMillimeters();
   if (sensor1.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
-  //delay(10);
+  delay(10);
   data.laserFront = sensor2.readRangeSingleMillimeters();
   if (sensor2.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
-  //delay(10);
+  delay(10);
   data.laserLeft = sensor3.readRangeSingleMillimeters();
   if (sensor3.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
   
@@ -285,7 +278,7 @@ bool CheckSide(int side, int dist)
       WriteMsg("Check Left Action", true);
       MoveDir(STRAFERIGHT);
       delay(25);
-      MoveDir(STOP);
+      MoveDir(FORWARD);
     }
   }
   else
@@ -295,36 +288,71 @@ bool CheckSide(int side, int dist)
       WriteMsg("Check Right Action", true);
       MoveDir(STRAFELEFT);
       delay(25);
-      MoveDir(STOP);
+      MoveDir(FORWARD);
     }
   }
 }
 
+void turn(int dir)
+{
+    MoveDir(FORWARD); 
+    delay(50);
+
+    if(dir == RIGHT){
+      MoveDir(dir);
+      delay(50);
+      
+      while(data.laserRight < 255){
+        MoveDir(dir);
+      }    
+    }
+    else if(dir == LEFT){
+      MoveDir(dir);
+      delay(50);
+      
+      while(data.laserLeft < 255){
+        MoveDir(dir);
+      }   
+    }
+}
+
 void WriteMsg(String msg, bool newLine)
 {
-//  if(newLine)
-//  {
-//    Serial.print(msg);
-//    Serial.print("\n");
-//  }
-//  else
-//  {
-//    Serial.print(msg);
-//    Serial.print("\t");
-//  }
+  if(newLine)
+  {
+    Serial.print(msg);
+    Serial.print("\n");
+  }
+  else
+  {
+    Serial.print(msg);
+    Serial.print("\t");
+  }
 }
 
 void WriteInt(int val, bool newLine)
 {
-//  if(newLine)
-//  {
-//    Serial.print(val);
-//    Serial.print("\n");
-//  }
-//  else
-//  {
-//    Serial.print(val);
-//    Serial.print("\t");
-//  }
+  if(newLine)
+  {
+    Serial.print(val);
+    Serial.print("\n");
+  }
+  else
+  {
+    Serial.print(val);
+    Serial.print("\t");
+  }
 }
+
+//void right_turn(int dist)
+//{
+//  MoveDir(FORWARD);
+//  delay(100);
+//  while (dist > laserActionThreshhold)
+//  {
+//    MoveDir(RIGHT);
+//    delay(100);
+//  }
+//  CheckSide(2,150);
+//}
 
